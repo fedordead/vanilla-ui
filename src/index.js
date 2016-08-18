f6a2b7f0d7516067db693b17f4b0d45bae2ef55f
DaveOrDead <davidajberner@gmail.com>
2016-08-18 06:21:07 +0100

    update let to const for initialisers in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import UIDialog from './components/dialog/dialog';
 
-let dialog1 = UIDialog();
-let dialog2 = UIDialog({
+const dialog1 = UIDialog();
+const dialog2 = UIDialog({
     dialog:         '.js-dialog-2',
     openBtn:        '.js-dialog-btn-2',
     closeBtn:       '.js-dialog-close-btn-2',
