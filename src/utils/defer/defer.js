de87de0d19a82fbebee64fd7d47bad5aab2880a0
DaveOrDead <davidajberner@gmail.com>
2016-08-25 06:29:28 +0100

    update comment on defer to include reference

--- src/utils/defer/defer.js
+++ src/utils/defer/defer.js
@@ -1,10 +1,17 @@
 /**
  * @function defer
- * @desc Prevents function triggering until completion
+ * @desc Pauses JS execution to let the rendering threads catch up.  This is the effect that
+ * setTimeout() with a timeout of 0 does. Although it seems to say "run this immediately" it
+ * actually gives the browser a chance to finish doing some non-JS things that have been
+ * waiting to finish before attending to this new piece of code.
+ * @credit http://stackoverflow.com/questions/779379/why-is-settimeoutfn-0-sometimes-useful
  * @param {function} func function to run
  */
 const defer = func => {
-    if (typeof func === 'function') setTimeout(func, 0);
+    if (typeof func === 'function') {
+        setTimeout(func, 0);
+    }
 };
+
 
 export default defer;
