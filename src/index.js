7e4260cc94116fce15705fae875e18e7712b0416
DaveOrDead <davidajberner@gmail.com>
2016-08-29 15:43:22 +0100

    remove aligned object props as not doing this elsewhere

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import UITypeahead from './components/typeahead';
 
 VUIDialog();
 VUIDialog({
-    dialog:         '.js-dialog-2',
-    openBtn:        '.js-dialog-btn-open-2',
-    closeBtn:       '.js-dialog-btn-close-2',
-    isModal:        true,
-    showBackdrop:   false
+    dialog: '.js-dialog-2',
+    openBtn: '.js-dialog-btn-open-2',
+    closeBtn: '.js-dialog-btn-close-2',
+    isModal: true,
+    showBackdrop: false
 });
 
 
